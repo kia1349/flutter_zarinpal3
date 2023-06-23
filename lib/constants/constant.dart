bea1e19c506fa5f5a82ba2f2711df08407031c4a
kia1349 <kia1349@yahoo.com>
2023-06-23 14:50:10 +0330

    Initial Commit

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -1,6 +1,6 @@
 class AppConstants {
-  static const String MERCHANTID="992a89cc-0139-11e6-ace4-005056a205be";
-  static const String MOBILE="09122398658";
+  static const String MERCHANTID="772a87cc-0137-11e6-ace4-005056a205be";
+  static const String MOBILE="07122378658";
   static const String EMAIL="kia1349@gmail.com";
   static const String theme = 'theme';
 }
